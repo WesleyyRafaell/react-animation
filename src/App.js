3a4e1dc35309e1489368222f3e106bb4ddefeee2
rafael <rafaelsanto10s@gmail.com>
2020-11-15 11:35:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Lottie from 'react-lottie';
+
+import animationData from './animation.json';
+
+import './global.css';
 
 function App() {
+  const [animationState, setAnimationState] = useState({
+    isStopped: false, isPaused: false
+  });
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containerAnimtion">
+      <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+              isStopped={animationState.isStopped}
+              isPaused={animationState.isPaused}/>
+      </div>
     </div>
   );
 }
